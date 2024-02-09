fdcb317edd49a0a6c30df1cad5eb11cbcc874908
becominn <nagase.mikito@classmethod.jp>
2024-02-09 13:50:28 +0900

    Initial commit

---
+++ test/security-learning-box.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SecurityLearningBox from '../lib/security-learning-box-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/security-learning-box-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SecurityLearningBox.SecurityLearningBoxStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
